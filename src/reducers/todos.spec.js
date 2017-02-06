8c5ba15fbc00f48c2d59238e1e863632d964d441
Mike Calvanese <mfcalvanese@gmail.com>
2017-02-06 14:33:29 -0500

    Refactor todos spec to use sazerac assertions

--- src/reducers/todos.spec.js
+++ src/reducers/todos.spec.js
@@ -1,111 +1,62 @@
+import { test, given } from 'sazerac'
 import todos from './todos'
 
-describe('todos reducer', () => {
-  it('should handle initial state', () => {
-    expect(
-      todos(undefined, {})
-    ).toEqual([])
+test(todos, () => {
+
+  given(undefined, {}).expect([])
+
+  given([], {
+    type: 'ADD_TODO',
+    text: 'Run the tests',
+    id: 0
+  })
+  .describe('when state is empty and a todo is added')
+  .assert('should add the todo and set `id` property', (state) => {
+    expect(state[0].id).toEqual(0)
+  })
+  .assert('should add the todo and set `text` property', (state) => {
+    expect(state[0].text).toEqual('Run the tests')
+  })
+  .assert('should set completed:false on the added todo', (state) => {
+    expect(state[0].completed).toEqual(false)
   })
 
-  it('should handle ADD_TODO', () => {
-    expect(
-      todos([], {
-        type: 'ADD_TODO',
-        text: 'Run the tests',
-        id: 0
-      })
-    ).toEqual([
-      {
-        text: 'Run the tests',
-        completed: false,
-        id: 0
-      }
-    ])
-
-    expect(
-      todos([
-        {
-          text: 'Run the tests',
-          completed: false,
-          id: 0
-        }
-      ], {
-        type: 'ADD_TODO',
-        text: 'Use Redux',
-        id: 1
-      })
-    ).toEqual([
-      {
-        text: 'Run the tests',
-        completed: false,
-        id: 0
-      }, {
-        text: 'Use Redux',
-        completed: false,
-        id: 1
-      }
-    ])
-
-    expect(
-      todos([
-        {
-          text: 'Run the tests',
-          completed: false,
-          id: 0
-        }, {
-          text: 'Use Redux',
-          completed: false,
-          id: 1
-        }
-      ], {
-        type: 'ADD_TODO',
-        text: 'Fix the tests',
-        id: 2
-      })
-    ).toEqual([
-      {
-        text: 'Run the tests',
-        completed: false,
-        id: 0
-      }, {
-        text: 'Use Redux',
-        completed: false,
-        id: 1
-      }, {
-        text: 'Fix the tests',
-        completed: false,
-        id: 2
-      }
-    ])
+  given([
+    {
+      text: 'Run the tests',
+      completed: false,
+      id: 0
+    }
+  ], {
+    type: 'ADD_TODO',
+    text: 'Use Redux',
+    id: 1
+  })
+  .describe('when state has an existing todo and another todo is added')
+  .assert('should add the todo after the existing todo', (state) => {
+    expect(state[1].id).toEqual(1)
   })
 
-  it('should handle TOGGLE_TODO', () => {
-    expect(
-      todos([
-        {
-          text: 'Run the tests',
-          completed: false,
-          id: 1
-        }, {
-          text: 'Use Redux',
-          completed: false,
-          id: 0
-        }
-      ], {
-        type: 'TOGGLE_TODO',
-        id: 1
-      })
-    ).toEqual([
-      {
-        text: 'Run the tests',
-        completed: true,
-        id: 1
-      }, {
-        text: 'Use Redux',
-        completed: false,
-        id: 0
-      }
-    ])
+  given([
+    {
+      text: 'Run the tests',
+      completed: false,
+      id: 1
+    }, {
+      text: 'Use Redux',
+      completed: false,
+      id: 0
+    }
+  ], {
+    type: 'TOGGLE_TODO',
+    id: 1
+  })
+  .describe('when a todo with completed:false is toggled')
+  .assert('should set complete:true on the toggled todo', (state) => {
+    expect(state[0].completed).toEqual(true)
+  })
+  .assert('should not toggle any other todos', (state) => {
+    expect(state[1].completed).toEqual(false)
   })
 
 })
